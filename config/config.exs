44aa659488e19bd26805f6147f851dd805f80d76
Aaron Seigo <aseigo@kde.org>
2018-08-03 23:07:36 +0200

    configure the logger

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,10 @@
 use Mix.Config
+
+config :logger,
+  backends: [{LoggerFileBackend, :error_log}]
+
+config :logger, :error_log,
+  path: "error.log",
+  format: "\n== $time\n$message",
+  level: :error
+
